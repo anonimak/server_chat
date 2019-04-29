3da8a5f89009b7fe3d4fcf7c6be49aaab49a06ec
root <root@localhost.localdomain>
2019-04-29 11:51:42 +0700

    Add existing file

---
+++ database/mysql2.js
@@ -0,0 +1,19 @@
+const config_db = require('../config/config_db');
+var mysql = require('mysql2');
+
+//var db = mysql.createConnection(config_db);
+var db = mysql.createPool(config_db);
+
+// db.connect(function (err) {
+//     if (err) {
+//         console.error('error connecting: ' + err.stack);
+//         return;
+//     }
+
+//     console.log('connected as id ' + db.threadId);
+// });
+
+module.exports = db.promise();
+
+
+// db.end();
